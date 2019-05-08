81fac6084c9261800b48b02aefd3a3c1b903cc19
Matttaylor8910 <taylor.matt777@gmail.com>
2019-05-08 14:15:04 -0400

    Commit mostly done items service

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+import { Item } from '../types';
+import * as _ from 'lodash';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemsService {
 
-  constructor() { }
+  constructor(
+    private afs: AngularFirestore
+  ) { 
+  }
+
+  addItem(partialItem: any) {
+
+    partialItem.createdAt = Date.now();
+    partialItem.createdBy = 'anonId';
+
+    return this.afs.collection('items').add({
+
+    })
+  }
+
+  getItem(itemId: string): Observable<Item> {
+    return this.afs.collection('items').doc(itemId)
+      .snapshotChanges()
+      .pipe(map(this.mapItems.bind(this)));
+  }
+
+  getItems(): Observable<Item[]> {
+    return this.afs.collection('items')
+      .snapshotChanges()
+      .pipe (
+        map(actions => {
+          return _(actions)
+            .map(this.mapItems.bind(this))
+            .value();
+        })
+      );
+  }
+
+  mapItems(item): Item {
+    const doc = item.payload.doc || item.payload;
+
+    const data = doc.data();
+    const id = doc.id;
+    const exists = doc.exists;
+
+    return { id, exists, ...data };
+  }
+
+  sanitize(incomingItem: Item): Item {
+    let item = _.clone(incomingItem);
+    return _.omit(item, ['exists', 'id']) as Item;
+  }
 }
