a2d691d5a96e7f407c215c0476211d93ac990555
jweaver <jweaver@relias.com>
2019-05-09 10:53:11 -0400

    clicking on a page item now pushes the page into the historyService

--- src/app/components/den-favorite-item/den-favorite-item.component.ts
+++ src/app/components/den-favorite-item/den-favorite-item.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Item} from "../../types";
+import {HistoryService} from "../../services/history.service";
+import {PagesService} from "../../services/pages.service";
 
 @Component({
   selector: 'den-favorite-item',
@@ -11,13 +13,21 @@ export class DenFavoriteItemComponent implements OnInit {
   //TODO this class is a dupe of den-item
   @Input() item: Item;
 
-  constructor() { }
+  constructor(
+    public historyService: HistoryService,
+    public pageService: PagesService) { }
 
   ngOnInit() {}
 
   itemClicked(item: Item) {
     switch (item.type) {
-      case 'page': console.log('page item clicked'); return;
+      case 'page':
+        this.pageService.getPage(item.pageId).toPromise().then( page => {
+          this.historyService.push(this.pageService.mapPages(page));
+        });
+        console.log('page item clicked');
+
+        break;
       case 'detail': console.log('detail item clicked'); break;
       case 'link': window.open(item.url); break;
       default: console.log("no idea what was clicked: "); return;
