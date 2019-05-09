be4bd5027cf1b5bd30f24c118c21f9dc31ad7390
jweaver <jweaver@relias.com>
2019-05-09 16:15:37 -0400

    added search icon to homescreen and added search modal to the project

---
+++ src/app/search-modal/search-modal.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-modal',
+  templateUrl: './search-modal.page.html',
+  styleUrls: ['./search-modal.page.scss'],
+})
+export class SearchModalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
