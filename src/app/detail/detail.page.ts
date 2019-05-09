e4101f5269e1d2b7fb4efb8b5859ea010ffc43e8
Warren Sutherland <wsutherland@relias.com>
2019-05-09 17:09:37 -0600

    chat is almost working.....

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {Item} from "../types";
+import {Item, Message} from "../types";
 import {ActivatedRoute} from "@angular/router";
 import {ItemsService} from "../services/items.service";
 import {UserService} from "../services/user.service";
+import {MessagesService} from "../services/messages.service";
 
 @Component({
   selector: 'app-detail',
@@ -14,24 +15,34 @@ export class DetailPage implements OnInit {
   id: string;
   item: Item;
   $item: any;
+  newMessage: string;
+  messages: Message[];
+  $messages: any;
 
   constructor(
       private route: ActivatedRoute,
       private itemsService: ItemsService,
-      private userService: UserService
+      private userService: UserService,
+      private messagesService: MessagesService
   ) {
     const sub = this.route.params.subscribe(params => {
       this.id = params['id'];
       sub.unsubscribe();
-      this.getItem();
     });
+    this.getItem();
+    this.newMessage = '';
   }
 
 
   ngOnInit() {
   }
 
+  clearMessage(){
+      this.newMessage = '';
+
+  }
   getItem(){
+  console.log(this.id);
     if(this.id){
       if(this.$item) this.$item.unsubscribe();
       this.$item = this.itemsService.getItem(this.id).subscribe( itm => {
@@ -40,9 +51,26 @@ export class DetailPage implements OnInit {
     }
   }
 
-  //Get a live update of change to item
+  addMessage(){
+    console.log(this.newMessage);
+    this.messagesService.addMessage(this.newMessage, this.id);
+    this.clearMessage();
+  }
+
+  canAddMessage(){
+     return this.newMessage.length > 1;
+  }
+
+  //Get a live update of change to item ( not that that can happen right now...) and messages
   ngOnChanges(){
+      console.log(this.id);
     this.getItem();
+    if(this.id){
+        if(this.$messages) this.$messages.unsubscribe();
+        this.$messages = this.messagesService.getMessages(this.item.id).subscribe( msgs => {
+            this.messages = msgs;
+        })
+    }
   }
 
 }
--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import {Observable} from "rxjs";
-import {Message} from "../types";
-import {map} from "rxjs/operators";
 import {AngularFirestore} from "@angular/fire/firestore";
+import {UserService} from "./user.service";
+import {Item, Message} from "../types";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 import * as _ from 'lodash';
 
 @Injectable({
@@ -11,19 +12,42 @@ import * as _ from 'lodash';
 export class MessagesService {
 
   constructor(
-      private afs: AngularFirestore
+      private afs: AngularFirestore,
+      private userService: UserService
   ) { }
 
-  getMessages(itemId): Observable<Message[]>{
-    return this.afs.collection('items/messages', ref => {
-      return ref.where('itemId', '==', itemId);
-    })
-        .snapshotChanges()
-        .pipe (
-            map(actions => {
-              return _(actions)
-                  .value();
-            })
-        );
-  }
+    addMessage(messageText: string, itemId){
+        const message: any = {
+            createdAt: Date.now(),
+            createBy: this.userService.user,
+            userName: this.userService.getUser().name,
+            text: messageText
+        }
+        return this.afs.collection('items').doc(itemId)
+            .collection('messages')
+            .add(message);
+    }
+
+    getMessages(id: string): Observable<Message[]>{
+      return this.afs.collection('items').doc(id)
+          .collection('messages')
+          .snapshotChanges()
+          .pipe(
+              map( actions => {
+                  return _(actions)
+                      .map(this.mapItems.bind(this))
+                      .value();
+              })
+          );
+    }
+
+    mapItems(message): Message {
+        const doc = message.payload.doc || message.payload;
+
+        const data = doc.data();
+        const id = doc.id;
+        const exists = doc.exists;
+
+        return { id, exists, ...data };
+    }
 }
