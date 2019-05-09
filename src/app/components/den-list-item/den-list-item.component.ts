d933a774f4d47df1b0210203580d6414753ae71e
Matttaylor8910 <taylor.matt777@gmail.com>
2019-05-09 11:06:01 -0400

    Merge conflict

--- src/app/components/den-list-item/den-list-item.component.ts
+++ src/app/components/den-list-item/den-list-item.component.ts
@@ -1,5 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Item} from "../../types";
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from '../../types';
+import { HistoryService } from 'src/app/services/history.service';
+import { PagesService } from 'src/app/services/pages.service';
 
 @Component({
   selector: 'den-list-item',
@@ -8,21 +10,28 @@ import {Item} from "../../types";
 })
 export class DenListItemComponent implements OnInit {
 
-  @Input()
-  item: Item;
+  @Input() item: Item;
 
-  constructor() { }
+  constructor (
+    public historyService: HistoryService,
+    public pageService: PagesService
+  ) { 
+  }
 
   ngOnInit() {}
 
   itemClicked(item: Item) {
     switch (item.type) {
-      case 'page': console.log('page item clicked'); return;
+      case 'page':
+        this.pageService.getPage(item.pageId).toPromise().then( page => {
+          this.historyService.push(this.pageService.mapPages(page));
+        });
+        console.log('page item clicked');
+
+        break;
       case 'detail': console.log('detail item clicked'); break;
-      case 'link': window.open(item.url); break;;
+      case 'link': window.open(item.url); break;
       default: console.log("no idea what was clicked: "); return;
     }
-
   }
-
 }
